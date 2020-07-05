de4943d2817923f39392d65dc0a59e0ff67cd0a0
Victor Bonnier <Vikkear@hotmail.com>
2020-07-05 22:19:37 +0200

    Added basic main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public bool isStart;
+    public bool isQuit;
+
+    void OnMouseUp()
+    {
+        if (isStart)
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        if (isQuit)
+        {
+            Application.Quit();
+        }
+    }
+
+}
