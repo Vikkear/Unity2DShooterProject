6b7e79c5cedfb9fbdfc5a52aefce6ed63d2bda36
Victor Bonnier <Vikkear@hotmail.com>
2020-07-06 02:10:58 +0200

    Made the bars follow the camera

--- Assets/Scripts/StaminaBar.cs
+++ Assets/Scripts/StaminaBar.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class StaminaBar : MonoBehaviour
 {
     private Transform bar;
+    private Camera cam;
 
-    void Start()
+    public float offset = 0f;
+
+
+    private void Start()
     {
         bar = transform.Find("Bar");
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        Vector3 screenVector = cam.transform.position;
+        transform.position = new Vector3(screenVector.x - 0.5f, screenVector.y - offset, 20);
     }
 
     public void setSize(float sizeNormalized)
